9a472e3771a465ab42f6383a3ac8d0642053ac4f
Anuj Yadav <91149204+yalrnr@users.noreply.github.com>
2021-10-14 00:33:50 +0530

    Hindu/Arabic Operations

---
+++ Hindu_or_Arabic_operations.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    printf("Enter two numbers for Sum and Product\n");
+    scanf("%d %d",&num1,&num2);
+    printf("The Sum of the two numbers is %d\nThe Product of the two numbers is %d \n",num1+num2,num1*num2);
+    return 0;
+}
